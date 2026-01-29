2e9be15d367d88ed533a6e41034e0aae558cc33f
Nicolas Rakela <nicorakela@bwrcrdsl-1.EECS.Berkeley.EDU>
2026-01-28 21:32:40 -0800

    make test mmio

--- bareMetalC/matmul_ws.c
+++ bareMetalC/matmul_ws.c
@@ -14,6 +14,26 @@
 // Match your header types
 #define DIM MATMUL_M
 
+#define GEMMINI_CTRL 0x40084000
+#define GEMMINI_RS1_ADDR (GEMMINI_CTRL + 0x10)
+#define GEMMINI_RS2_ADDR (GEMMINI_CTRL + 0x18)
+#define GEMMINI_INST_ADDR (GEMMINI_CTRL + 0x0)
+
+#undef ROCC_INSTRUCTION_RS1_RS2
+#define ROCC_INSTRUCTION_RS1_RS2(x, rs1, rs2, funct) { \
+    *((volatile uint64_t *) GEMMINI_RS1_ADDR) = (rs1); \
+    *((volatile uint64_t *) GEMMINI_RS2_ADDR) = (rs2); \
+    *((volatile uint32_t*) GEMMINI_INST_ADDR) = (0x7B) | (0 << 7) | (3 << 12) | (1 << 15) | (2 << 20) | ((funct) << 25); \
+}
+
+#define k_MVIN         2
+#define k_MVOUT        3
+#define k_MVOUT_SPAD 23
+#define XCUSTOM_ACC 3
+#define ADDR_LEN 32
+#define DIM 16
+
+
 typedef uint8_t elem_t;   // A_in: lower 8 bits = fp8:e4m3, upper bits zero
 typedef uint8_t welem_t;  // B_in: lower 8 bits = fp8:e4m3, upper bits zero
 typedef uint64_t  out_t;    // C_scaled: fp8:e4m3 (1 byte per output)
